d2d8086acf0e8dc87add3b11b50663edac26d421
unknown <Jeremy Greer@LPT-CNU216063X.celerant.com>
2012-11-30 09:16:43 -0500

    multiple tasks on each task line

--- js/timecard.js
+++ js/timecard.js
@@ -15,140 +15,7 @@ export as JSON
 /**
 * @param {object} data
 **/
-function Task(data){
-	var start = new Date(),
-		end,
-		label = 'new task',
-		color,
-		element,
-		instance = this;
 
-	if(data){
-		start = new Date(data.start);
-		end = data.end ? new Data(data.end) : undefined;
-		label = data.label;
-		color = data.color;
-	};
-
-
-	element = $('<div>')
-		.addClass('task')
-		.html(label)
-		.click(function(e){
-			
-			var input = $('<input>')
-				.click(function(e){
-					e.stopPropagation();
-				})
-
-				.blur(function(){
-
-					var newVal = $.trim($(this).val());
-
-					if(newVal !== ''){
-						
-						instance.setLabel(newVal);
-					};
-					
-					$(this).remove();
-				});
-
-
-			e.stopPropagation();
-
-			$(this).append(input);
-
-			input.focus();
-		})
-		.data('task', instance);
-
-
-	/**
-	* @param {Date} time
-	* @return {boolean} success
-	**/
-	this.setStart = function(time){
-		
-		if(end === undefined || time < end){
-			start = time;
-			return true;
-		};
-
-		return false;
-	};
-
-	this.getStart = function(){
-		return start;
-	};
-
-	/**
-	* @param {Date} time
-	* @return {boolean} success
-	**/
-	this.setEnd = function(time){
-
-		var time = time || new Date();
-		
-		if(time > start){
-			end = time;
-			return true;
-		};
-
-		return false;
-	};
-
-	this.getEnd = function(){
-		return end;
-	};
-
-	this.setLabel = function(newLabel){
-		label = newLabel;
-		element.html(newLabel);
-	};
-
-	this.getLabel = function(){
-		return label;
-	};
-
-	this.toString = function(){
-		var friendlyStart = getFriendlyTime(start),
-			friendlyEnd = (end === undefined) ? '...' : getFriendlyTime(end);
-
-		return friendlyStart + ' - ' + friendlyEnd + ' (' + label + ')';
-
-		function getFriendlyTime(time){
-			var h = pad(time.getHours()),
-				m = pad(time.getMinutes()),
-				s = pad(time.getSeconds());
-
-			return h + ':' + m + ':' + s;
-
-			function pad(x){
-
-				//	allow type conversion
-				if(x < 10){
-					return '0' + x;
-				};
-
-				return x;
-			};
-		};
-	};
-
-	this.getData = function(){
-		
-		return {
-			start: start,
-			end: end,
-			label: label
-			//color: color
-		};
-	};
-
-	this.getElement = function(){
-		return element;
-	};
-};
 
 
 
@@ -189,6 +56,7 @@ var taskGraph,
 	taskManager = new TaskManager();
 
 $(function(){
+	/*
 
 	var taskLineAdder = new TaskLine().getElement().click(addTaskLine).html('click for new task');
 
@@ -344,12 +212,13 @@ $(function(){
 	
 
 //	clearInterval(ticInterval);
-
+*/
 
 
 	/******************/
 	(function(){
 		var taskGraph = new TaskGraph($('#test'));
+
 	})();
 
 
@@ -357,86 +226,319 @@ $(function(){
 
 
 function TaskGraph(element){
-	var element = element,
+	var taskGraphElement = element,
 		newTaskButton,
+		summaryButton,
+		controls,
 		taskLines = [];
 
 	//	set css
-	element.css({
+	taskGraphElement.css({
 		position: 'relative',
-		width: '500px',
+		width: '800px',
 		height: '300px'
 	});
 
-	element.addClass('taskGraph');
+	taskGraphElement.addClass('taskGraph');
 
 	//	new task button
 	newTaskButton = $('<button>')
-		.html('new task')
-		.click(function(){
-			addTask();
-		});
+		.text('new task')
+		.click(addTaskLine);
 
-	element.before(newTaskButton);
+	//	summary button
+	summaryButton = $('<button>')
+		.text('summary')
+		.click(showSummary);
 
-	function addTask(){
+	//	wrapper for controls
+	controls = $('<div>').append(newTaskButton, summaryButton);
+	taskGraphElement.before(controls);
+
+	function addTaskLine(){
 		var taskLine = new TaskLine();
 
 		taskLines.push(taskLine);
-		element.prepend(taskLine.getElement());
+		taskGraphElement.prepend(taskLine.getElement());
+	};
+
+	function showSummary(){
+		var summary = [];
+
+		for(var i = 0; i < taskLines.length; i++){
+			summary.push( taskLines[i].getSummary() );
+		};
+
+		summary = JSON.stringify(summary);
+		localStorage.summary = summary;
+		console.log(summary);
+	};
+
+	function TaskLine(){
+		var taskLineElement,
+			timeline,
+			controls,
+			toggle,
+			label,
+			tasks = [];
+
+		taskLineElement = $('<div>')
+			.addClass('taskLine');
+
+		timeline = $('<div>').addClass('timeline');
+		controls = $('<div>').addClass('controls');
+
+		//	initialize
+		addTask();
+		toggle = new Toggle();
+		label = new Label();
+
+		controls.append(
+			toggle.getElement(),
+			label.getElement()
+		);
+
+		//	add components
+		taskLineElement.append(timeline, controls);
+
+		function addTask(){
+			var task = new Task();
+			tasks.push(task);
+
+			timeline.append(task.getElement());
+		};
+
+		function Toggle(){
+
+			var toggleElement,
+				playing = true;
+
+			toggleElement = $('<div>')
+				.addClass('toggle')
+				.text('pause')
+				.click(toggle);
+
+			function toggle(){
+
+				var newestTask = tasks[tasks.length - 1];
+
+				if(playing){
+					toggleElement.text('play');
+				} else {
+					toggleElement.text('pause');
+				};
+
+				if(newestTask.getEnd() === undefined){
+					newestTask.setEnd();
+				} else {
+					//	create a new task (to the user, this looks like resuming)
+					addTask();
+				};
+
+				playing = !playing;
+			};
+
+			function getElement(){
+				return toggleElement;
+			};	
+
+			return {
+				getElement: getElement
+			};
+		};
+
+
+		function Label(){
+
+			var labelElement,
+				label = 'label';
+
+			labelElement = $('<div>')
+				.addClass('label')
+				.click(function(){
+
+					var input,
+						oldLabel;
+
+					oldLabel = $(this).text();
+
+					$(this).empty();
+
+					input = $('<input>')
+						.val(oldLabel)
+						.click(function(e){
+							e.stopPropagation();
+						})
+
+						.blur(function(){
+
+							var newVal = $.trim($(this).val());
+
+							$(this).remove();
+
+							if(newVal !== ''){
+								setLabel(newVal);
+							} else {
+								setLabel(oldLabel);
+							};
+						});
+
+					input
+						.appendTo(labelElement)
+						.focus();
+				});
+
+			//	set initial label
+			setLabel(label);
+
+			function setLabel(newLabel){
+				label = newLabel;
+				labelElement.text(label);
+			};
+
+			function getLabel(){
+				return label;
+			};
+
+			function getElement(){
+				return labelElement;
+			};
+
+			return {
+				setLabel: setLabel,
+				getLabel: getLabel,
+				getElement: getElement
+			};
+		};
+
+
+		function getElement(){
+			return taskLineElement;
+		};
+
+		//	return object
+		function getSummary(){
+			var summary = {
+				label: label.getLabel(),
+				tasks: []
+			};
+
+			for(var i = 0; i < tasks.length; i++){
+				summary.tasks.push( tasks[i].getSummary() );
+			};
+
+			return summary;
+		};
+
+		return {
+			getElement: getElement,
+			getSummary: getSummary
+		};
+	};
+
+
+	function Task(data){
+		var start = new Date(),
+			end,
+			duration,
+			taskElement;
+
+		if(data){
+			start = new Date(data.start);
+			end = data.end ? new Data(data.end) : undefined;
+		};
+
+		taskElement = $('<div>')
+			.addClass('task')
+			.click(function(e){
+				e.stopPropagation();
+			});
+
+		/**
+		* @param {Date} time
+		* @return {boolean} success
+		**/
+		function setStart(time){
+			
+			if(end === undefined || time < end){
+				start = time;
+				return true;
+			};
+
+			return false;
+		};
+
+		function getStart(){
+			return start;
+		};
+
+		/**
+		* @param {Date} time
+		* @return {boolean} success
+		**/
+		function setEnd(time){
+
+			var time = time || new Date();
+			
+			if(time > start){
+				end = time;
+				
+				//	record duration
+				duration = Math.round((end.getTime() - start.getTime()) / 1000);
+
+				if( !isNaN(duration) ){
+					duration = convertSecondsToTime(duration);
+				};
+
+				return true;
+			};
+
+			return false;
+		};
+
+		function getEnd(){
+			return end;
+		};
+
+		function getSummary(){
+
+			return {
+				start: start,
+				end: end,
+				duration: duration
+			};
+		};
+
+		function getElement(){
+			return taskElement;
+		};
+
+		return {
+			getElement: getElement,
+			getSummary: getSummary,
+			getEnd: getEnd,
+			setEnd: setEnd
+		};
 	};
 };
 
-function TaskLine(){
-	var element,
-		timeline,
-		controls,
-		label = 'new task',
-		tasks = [];
+function getFriendlyTime(time){
+	var h = pad(time.getHours()),
+		m = pad(time.getMinutes()),
+		s = pad(time.getSeconds());
 
-	element = $('<div>')
-		.addClass('taskLine');
+	return h + ':' + m + ':' + s;
 
-	timeline = $('<div>').addClass('timeline');
-	controls = $('<div>').addClass('controls');
+	function pad(x){
 
-	controls.append(
-		$('<div>')
-			.addClass('controls')
-			.html('toggle'),
+		//	allow type conversion
+		if(x < 10){
+			return '0' + x;
+		};
 
-		new Label(label)
-	);
-
-	//	add components
-	element.append(timeline, controls);
-
-	//	add an initial task
-	addTask();
-
-	function addTask(){
-		var task = new Task();
-		tasks.push(task);
-
-		timeline.append(task.getElement());
-	};
-
-	function Label(label){
-
-		var element = $('<div>')
-			.addClass('label')
-			.html(label)
-
-		return element;
-	};
-
-	//element.append(task.getElement());
-
-	this.getElement = function(){
-		return element;
+		return x;
 	};
 };
-
 
 function convertSecondsToTime(sec){
 		var h,
