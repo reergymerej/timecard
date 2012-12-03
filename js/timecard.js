165c0e9a6b364f18ce9c67561367ec2baf445432
Jeremy Greer <jeremygreer@reergymerej.com>
2012-12-03 00:26:59 -0500

    add task time modifier

--- js/timecard.js
+++ js/timecard.js
@@ -74,6 +74,104 @@ function convertSecondsToTime(sec){
 };
 
 
+/**
+* [m/d[/y] ]h24[:mi[:s]]
+* @return {Date}
+**/
+function convertUserInputToDate(x){
+	//	new Date(year, month, day [, hour, minute, second, millisecond])
+	
+	var parts = [],
+		dayPart,
+		timePart;
+
+	//	What did they provide?
+	if(x.indexOf(' ') !== -1){
+		parts = x.split(' ');
+		dayPart = parts[0];
+		timePart = parts[1];
+
+	} else if(x.indexOf('/') !== -1) {
+		dayPart = x;
+	} else {
+		timePart = x;
+	};
+
+	//	complete the parts
+	dayPart = getDayPart(dayPart);
+	timePart = getTimePart(timePart);
+
+	return new Date(dayPart.y, dayPart.m, dayPart.d, timePart.h, timePart.mi, timePart.s);
+
+	/**
+	* @return {object.m}
+	* @return {object.d}
+	* @return {object.y}
+	**/
+	function getDayPart(dayPart){
+		var m, d, y, 
+			parts = [],
+			now = new Date();
+
+		//	set defaults
+		m = now.getMonth() + 1;
+		y = now.getFullYear();
+		d = now.getDate();
+
+		if(dayPart){
+			
+			parts = dayPart.split('/');
+			m = parts[0];
+			d = parts[1];
+
+			//	they provided a year, too
+			if(parts.length === 3){
+				y = parts[2];
+			};
+		};
+		
+		return {
+			m: m - 1,
+			d: d,
+			y: y
+		};
+	};
+
+	/**
+	* @return {object.h}
+	* @return {object.mi}
+	* @return {object.s}
+	**/
+	function getTimePart(timePart){
+		var h, mi, s, 
+			parts = [],
+			now = new Date();
+
+		//	set defaults
+		h = 0;
+		mi = 0;
+		s = 0;
+
+		if(timePart){
+			
+			parts = timePart.split(':');
+			h = parts[0];
+			mi = parts[1] || 0;
+
+			//	they provided seconds, too
+			if(parts.length === 3){
+				s = parts[2];
+			};
+		};
+
+		return {
+			h: h,
+			mi: mi,
+			s: s
+		};
+	};
+};
+
 $(function(){
 	var taskGraph = new TaskGraph($('#test'));
 });
@@ -107,113 +205,15 @@ function TaskGraph(element){
 	$('#summary_form')
 		.submit(function(){
 			var start = $('#start').val(),
-				end = $('#end').val();
+				end = $('#end').val() || '24';
 
 			//	convert these values into dates
-			start = convertToDate(start);
-			end = convertToDate(end);
+			start = convertUserInputToDate(start);
+			end = convertUserInputToDate(end);
 
 			showSummary(start, end);
 			
 			return false;
-
-			/**
-			* [m/d[/y] ]h24[:mi[:s]]
-			* @return {Date}
-			**/
-			function convertToDate(x){
-				//	new Date(year, month, day [, hour, minute, second, millisecond])
-				
-				var parts = [],
-					dayPart,
-					timePart;
-
-				//	What did they provide?
-				if(x.indexOf(' ') !== -1){
-					parts = x.split(' ');
-					dayPart = parts[0];
-					timePart = parts[1];
-
-				} else if(x.indexOf('/') !== -1) {
-					dayPart = x;
-				} else {
-					timePart = x;
-				};
-
-				//	complete the parts
-				dayPart = getDayPart(dayPart);
-				timePart = getTimePart(timePart);
-
-				return new Date(dayPart.y, dayPart.m, dayPart.d, timePart.h, timePart.mi, timePart.s);
-
-				/**
-				* @return {object.m}
-				* @return {object.d}
-				* @return {object.y}
-				**/
-				function getDayPart(dayPart){
-					var m, d, y, 
-						parts = [],
-						now = new Date();
-
-					//	set defaults
-					m = now.getMonth() + 1;
-					y = now.getFullYear();
-					d = now.getDate();
-
-					if(dayPart){
-						
-						parts = dayPart.split('/');
-						m = parts[0];
-						d = parts[1];
-
-						//	they provided a year, too
-						if(parts.length === 3){
-							y = parts[2];
-						};
-					};
-					
-					return {
-						m: m - 1,
-						d: d,
-						y: y
-					};
-				};
-
-				/**
-				* @return {object.h}
-				* @return {object.mi}
-				* @return {object.s}
-				**/
-				function getTimePart(timePart){
-					var h, mi, s, 
-						parts = [],
-						now = new Date();
-
-					//	set defaults
-					h = 0;
-					mi = 0;
-					s = 0;
-
-					if(timePart){
-						
-						parts = timePart.split(':');
-						h = parts[0];
-						mi = parts[1] || 0;
-
-						//	they provided seconds, too
-						if(parts.length === 3){
-							s = parts[2];
-						};
-					};
-
-					return {
-						h: h,
-						mi: mi,
-						s: s
-					};
-				};
-			};
 		});
 
 	saveButton = $('<button>')
@@ -541,7 +541,8 @@ function Task(data){
 		end,
 		duration,
 		taskElement,
-		category;
+		category,
+		instance;
 
 	if(data){
 		start = new Date(data.start);
@@ -552,6 +553,50 @@ function Task(data){
 		.addClass('task')
 		.click(function(e){
 			e.stopPropagation();
+
+			console.log('pause graph adjustment until this is over');
+			//	showTimeAdjuster
+			$(this).append(
+				$('<form>')
+					.addClass('timeAdjuster')
+					.append( newInput('start'), newInput('end'), $('<input>', {type: 'submit'}) )
+					.submit(function(e){
+						var start = $('#start', this).val(),
+							end = $('#end', this).val();
+
+						if(start) {
+							start = convertUserInputToDate(start);
+							setStart(start);
+						};
+
+						if(end){
+							end = convertUserInputToDate(end);
+							setEnd(end);
+						};
+
+						$(this).remove();
+						return false;
+					})
+					.click(function(e){
+						//	form submission triggers a click
+						e.stopPropagation();
+					})
+					.blur(function(){
+						console.log('blur');
+					})
+			);
+
+			//	focus on first input
+			$(this).find('input').first().focus();
+
+			function newInput(id, blur){
+				var input = $('<input>', {id: id})
+					.click(function(e){
+						e.stopPropagation();
+					});
+
+				return input;
+			};
 		});
 
 	/**
@@ -795,9 +840,6 @@ function HistoryProxy(location){
 	* @param {Date} [end]
 	**/
 	function getTasksFromHistory(start, end){
-
-		console.log(start, end);
-
 		return getTasks(start, end);
 	};
 
