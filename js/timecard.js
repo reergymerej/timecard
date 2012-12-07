4cb6a4dfc386b1b71929214f5d2251b59f4c60d5
Jeremy Greer <Jeremy Greer@LPT-CNU216063X.celerant.com>
2012-12-07 09:02:10 -0500

    enhance ui

--- js/timecard.js
+++ js/timecard.js
@@ -1,5 +1,20 @@
 $(function(){
 	require(['recorder'], function(recorder){
+
+		//	set up components, event listeners handled in modules
+		$( '#refresh-interval').slider({
+			min: 1,
+			max: 60,
+			value: 10
+		});
+
+		$( '#save-interval' ).slider({
+			min: 5,
+			max: 120,
+			value: 5,
+			step: 5
+		});
+
 		recorder.start();
 	});
 });
